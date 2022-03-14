04abbc82025f41eb96a97b4eb253ea817b72efd2
Jeshua Suarea-Lugo <jeshua13@live.com>
2022-03-14 17:18:03 -0500

    Courtesy message

--- xcapist.github.io/src/App.js
+++ xcapist.github.io/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Project in Progress. Please visit some time later.
         </p>
         <a
           className="App-link"
