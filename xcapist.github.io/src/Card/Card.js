98baf86ddcde24f5f75e7b9e703cf8369d44bc51
Jeshua Suarea-Lugo <jeshua13@live.com>
2022-03-15 22:28:44 -0500

    last minute change for clarity's sake

--- xcapist.github.io/src/Card/Card.js
+++ xcapist.github.io/src/Card/Card.js
@@ -3,6 +3,7 @@ import './Card.css';
 function Card(props) {
     return (
         <div className="card">{props.name}
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsam incidunt culpa facere assumenda quas mollitia, hic amet laborum tempora repudiandae veniam dolorem accusantium. Quas neque voluptate commodi tempora autem?</h5>
         </div>
     );
 }
